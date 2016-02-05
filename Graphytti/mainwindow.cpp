cb8fbee68950e707cb7a392e4e7b6a390d4a0c0a
Anish V Monsy <anishvmonsy2@gmail.com>
2016-02-06 00:35:37 +0530

    Enable QFileDialog to choose file

--- Graphytti/mainwindow.cpp
+++ Graphytti/mainwindow.cpp
@@ -12,3 +12,20 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_file_button_clicked()
+{
+
+    QString fileName= QFileDialog::getOpenFileName(this,
+                                                       tr("Open a file"),
+                                                       QDir().rootPath(),
+                                                       "All files (*)");
+    if(fileName.isEmpty()){
+        QMessageBox::information(this,tr("No file selected"),"Please choose a file to continue");
+    }
+    else{
+        QMessageBox::information(this,tr("Filename"),fileName);
+    }
+
+}
