e86d715a659287ccbd7c367f087c1e3dc8bda49c
likhith208 <pallaganilikhith@gmail.com>
2016-04-24 12:16:06 +0530

    Add Help Menu

--- Graphytti/mainwindow.cpp
+++ Graphytti/mainwindow.cpp
@@ -25,15 +25,66 @@ MainWindow::MainWindow(QWidget *parent)
     this->setCentralWidget(stackedWidget);
 
     //signals and slots connection
-    connect(excitationAnalysis, &QPushButton::clicked, this, &MainWindow::graphChoice);
+    connect(excitationAnalysis, &QPushButton::clicked, this, &MainWindow::excitationIntensityChoice);
+    createAction();
+    createMenu();
+}
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About Menu"),
+               tr("<b>GRAPHYTTI</b><br> "
+                  "This project is aimed at implementing an GUI interface for easily visualising"
+                  " the results of analysis after the fluorescence intensity decay experiment.<br>"
+                  "<br>"
+                  "<b>Supported Platforms</b><br>"
+                  "* Windows 7 or later<br>"
+                  "* Ubuntu Linux 14.04 (32-bit and 64-bit) or later <br>"
+                  "* Mac OS X 10.7 or later<br>"
+                  "<br>"
+                  "<a href = 'https://gitlab.com/anishvmonsy/team17cs243/blob/master/DEPENDENCIES.md'> Depencencies  </a><br>"
+                  "<br>"
+                  "<b>Copyright (C) 2015  Anish V Monsy,Vivek Datir,Mohd Imran,Likhith Pallagani</b><br>"
+                  "This program is free software: you can redistribute it and/or modify"
+                   " it under the terms of the GNU General Public License as published by"
+                   " the Free Software Foundation, either version 3 of the License, or"
+                   " (at your option) any later version."
+                   "<br>"
+                   "This program is distributed in the hope that it will be useful,"
+                  " but WITHOUT ANY WARRANTY; without even the implied warranty of"
+                  " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+                  " GNU General Public License for more details.<br>"
+                  "<br>"
+                  "Please see <a href ='http://www.gnu.org/licenses/gpl-3.0.en.html'>License</a>for an overview of licensing<br>"
+                  "Please see<a href = 'https://github.com/likhith208/Graphytti'>Code </a>for an overview of Source Code<br>"
+                  ));
 
 }
+void MainWindow::aboutQt()
+{
 
+
+}
+void MainWindow::createAction()
+{
+        aboutAct = new QAction(tr("&About"), this);
+        aboutAct->setStatusTip(tr("Shows about"));
+        connect(aboutAct, &QAction::triggered, this, &MainWindow::about);
+        aboutQtAct = new QAction(tr("About &Qt"), this);
+        aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
+        connect(aboutQtAct, &QAction::triggered, qApp, &QApplication::aboutQt);
+        connect(aboutQtAct, &QAction::triggered, this, &MainWindow::aboutQt);
+
+}
+void MainWindow::createMenu(){
+    helpMenu = menuBar()->addMenu(tr("&Help"));
+    helpMenu->addAction(aboutAct);
+    helpMenu->addAction(aboutQtAct);
+}
 MainWindow::~MainWindow()
 {
 
 }
-void MainWindow::graphChoice(){
+void MainWindow::excitationIntensityChoice(){
 
     myGraphPlotter= new ExpOnePlotter(stackedWidget);
     QString file_path= QFileDialog::getOpenFileName(this,
--- Graphytti/mainwindow.h
+++ Graphytti/mainwindow.h
@@ -4,18 +4,69 @@
 #include <QMainWindow>
 #include<QStackedWidget>
 #include "exponeplotter.h"
+class QMenu;
+class QAction;
 
+/**
+ * @brief
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 private:
+    /**
+      * @brief reference to the qstackedwidget present in main window
+      */
      QStackedWidget* stackedWidget;
+     /**
+        * @brief responsible for plotting  graphs and displaying other details of particular file format
+        */
        ExpOnePlotter *myGraphPlotter;
+       /**
+        * @brief creates action for menuBar()
+        */
+       void createAction();
+             /**
+               * @brief creates menubar
+               */
+              void createMenu();
+              /**
+               * @brief creates helpmenu in menubar
+               */
+              QMenu *helpMenu;
+              /**
+               * @brief creates "about" action in help menu
+               */
+              QAction *aboutAct;
+              /**
+               * @brief creates "aboutQt" action in help menu
+               */
+              QAction *aboutQtAct;
 private slots:
-    void graphChoice();
+   /**
+     * @brief slot activated when user chooses excitation intensity analyis
+     */
+    void excitationIntensityChoice();
+    /**
+     * @brief changes display back  to the main screen
+     */
     void mainScreen();
+    /**
+     * @brief slot activated when myGraphPlotter needs to display previous Cycle
+     */
     void prevCycle();
+    /**
+     * @brief slot activated when myGraphPlotter needs to display next Cycle
+     */
     void nextCycle();
+    /**
+     * @brief slot to simulate "about" action
+     */
+    void about();
+    /**
+     * @brief slot to simulate "about Qt" action
+     */
+    void aboutQt();
 
 
 public:
